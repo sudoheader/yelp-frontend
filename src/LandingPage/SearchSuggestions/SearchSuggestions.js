fca35f8b9e0b4ffd4ab38ee8509434854a676280
Raymond Nazaryan <sudoheader@users.noreply.github.com>
2021-07-25 17:26:23 -0700

    adapted layout and some other additions

--- src/LandingPage/SearchSuggestions/SearchSuggestions.js
+++ src/LandingPage/SearchSuggestions/SearchSuggestions.js
@@ -5,6 +5,9 @@ export function SearchSuggestions() {
     return (
         <div className={styles.suggestions}>
             <span className="icon is-small"><i className="fas fa-utensils"></i></span><span className={styles.suggestion}>Restaurants</span>
+            <span className="icon is-small"><i className="fas fa-cocktail"></i></span><span className={styles.suggestion}>Nightlife</span>
+            <span className="icon is-small"><i className="fas fa-concierge-bell"></i></span><span className={styles.suggestion}>Services</span>
+            <span className="icon is-small"><i className="fas fa-truck"></i></span><span className={styles.suggestion}>Delivery</span>
         </div>
     );
 };
